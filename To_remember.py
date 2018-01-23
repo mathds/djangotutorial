91f8090e6729ce2865b966f0fdfdd7600e2cb583
Mathieu <mdesahb@yahoo.fr>
2018-01-23 19:08:57 +0100

    Changing gitignore before cloning repo to PythonAnywhere

--- To_remember.py
+++ To_remember.py
@@ -2,64 +2,119 @@
  Django tutorial reminder
 """
 
+#### commenter ctrl-K decommenter ctrl-Q
 
-## Virtual environnement 
+### Virtual environnement 
 
 # cd C:/Users/nico/Documents/00_LBL/01_Tutorials/djangotutorial
 
-# What is it : 
+## What is it : 
 
-# It isolate your Python/Django setup on a per-project basis. 
-# This means that any changes you make to one website won't affect any 
-# others you're also developing.
+# # It isolate your Python/Django setup on a per-project basis. 
+# # This means that any changes you make to one website won't affect any 
+# # others you're also developing.
 
-# Create new virtual environnement
+# # Create new virtual environnement
 
 # python3 -m venv myvenv
 
-# Working with virtual environnement 
+# # Working with virtual environnement 
 
 # myvenv\Scripts\activate
 
 
 
 
-## Create Django project (don't forget the point, means in this directory) 
+# # Create Django project (don't forget the point, means in this directory) 
 
 # django-admin.exe startproject mysite .
 
-# manage.py is a script that helps with management of the site
+# # manage.py is a script that helps with management of the site
 
-# settings.py file contains the configuration of your website. TO CHANGE
+# # settings.py file contains the configuration of your website. TO CHANGE
 
-# create a database : we need to be in the directory that contains the manage.py file
+# # create a database : we need to be in the directory that contains the manage.py file
  
 # python manage.py migrate
 
-# starting the web server : in the directory that contains the manage.py file
+# # starting the web server : in the directory that contains the manage.py file
 
 # python manage.py runserver
 
-# create a separate application inside our project 
+# # create a separate application inside our project 
 
 # python manage.py startapp blog
 
-# then add blog into setting.py > INSTALLED_APPS
+# # then add blog into setting.py > INSTALLED_APPS
 
-# create models in app/models.py 
+# # create models in app/models.py 
 
-# create new model in DB :
+# # create new model in DB :
 
 # python manage.py makemigrations blog
 # python manage.py migrate blog
 
-# To add, edit and delete the posts we've just modeled, we will use Django admin.
-# In app/admin.py
+# # To add, edit and delete the posts we've just modeled, we will use Django admin.
+# # In app/admin.py
 
-# create superuser :
+# # create superuser :
 
 # python manage.py createsuperuser
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Git 
+
+# # Initialize repository
+
+# $ git init
+# Initialized empty Git repository in ~/djangogirls/.git/
+# $ git config --global user.name "Your Name"
+# $ git config --global user.email you@example.com
+
+# # check status 
+
+# git status
+
+# # add untracked files 
+
+# git add --all 
+
+# # commit all 
+
+# git commit -m
+
+# # hook up git repository to the website and push
+
+# $ git remote add origin https://github.com/<your-github-username>/my-first-blog.git
+# $ git push -u origin master
+
+## gitignore
+# *.pyc
+# *~
+# __pycache__
+# myvenv
+# db.sqlite3
+# /static
+# .DS_Store
 
 
 
